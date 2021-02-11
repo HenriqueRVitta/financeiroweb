1cceafc14a654c8a6d86971e94a1a3157d548e2e
HenriqueRVitta <suporte@hrvinformatica.com.br>
2021-02-11 12:06:45 -0300

    Ajuste gitignore

--- includes/conn.php
+++ includes/conn.php
@@ -7,6 +7,13 @@ $dbuser     = "root";
 $dbpass     = "vitta";
 $dbname_main = "hrvinfor_adm";
 
+/*
+$dbhost     = "localhost";
+$dbname     = "hrvinfor_adm";
+$dbuser     = "hrvinfor_vitta";
+$dbpass     = "hvitta2013";
+$dbname_main = 'hrvinfor_adm';
+*/
 
 // database connection
 try {
